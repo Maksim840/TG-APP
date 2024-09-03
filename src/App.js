f4c56409e267c36c5a6cc2291fb8cec609fa4ac0
maksimmiksha <@Maksim840>
2024-09-04 02:04:37 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
 const tg = window.Telegram.WebApp
-function App() {
   function App() {
     const onClose = () => {
       
   }
-  
+
   return (
     <div className="App">
       sdfsdf
